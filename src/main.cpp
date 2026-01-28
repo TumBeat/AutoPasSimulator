2c3b28494d0d18155e770c4422536aa094fdaf87
Luis Gall <luis.gall@tum.de>
2026-01-28 14:57:47 +0100

    Removed auto also from forEach lambdas

--- src/main.cpp
+++ src/main.cpp
@@ -59,14 +59,14 @@ int main(int argc, char** argv) {
             Kokkos::Profiling::pushRegion("Position Update");
             autoPasInstance.forEachKokkos<ForEachSpace::execution_space>(KOKKOS_LAMBDA(int i, const autopas::utils::KokkosStorage<KokkosParticle>& storage) {
 
-                const auto mass = storage.operator()<KokkosParticle::AttributeNames::mass, true, forEachHostFlag>(i);
-                auto vX = storage.operator()<KokkosParticle::AttributeNames::velocityX, true, forEachHostFlag>(i);
-                auto vY = storage.operator()<KokkosParticle::AttributeNames::velocityY, true, forEachHostFlag>(i);
-                auto vZ = storage.operator()<KokkosParticle::AttributeNames::velocityZ, true, forEachHostFlag>(i);
+                const typename KokkosParticle::ParticleSoAFloatPrecision mass = storage.operator()<KokkosParticle::AttributeNames::mass, true, forEachHostFlag>(i);
+                typename KokkosParticle::ParticleSoAFloatPrecision vX = storage.operator()<KokkosParticle::AttributeNames::velocityX, true, forEachHostFlag>(i);
+                typename KokkosParticle::ParticleSoAFloatPrecision vY = storage.operator()<KokkosParticle::AttributeNames::velocityY, true, forEachHostFlag>(i);
+                typename KokkosParticle::ParticleSoAFloatPrecision vZ = storage.operator()<KokkosParticle::AttributeNames::velocityZ, true, forEachHostFlag>(i);
 
-                auto fX = storage.operator()<KokkosParticle::AttributeNames::forceX, true, forEachHostFlag>(i);
-                auto fY = storage.operator()<KokkosParticle::AttributeNames::forceY, true, forEachHostFlag>(i);
-                auto fZ = storage.operator()<KokkosParticle::AttributeNames::forceZ, true, forEachHostFlag>(i);
+                typename KokkosParticle::ParticleSoAFloatPrecision fX = storage.operator()<KokkosParticle::AttributeNames::forceX, true, forEachHostFlag>(i);
+                typename KokkosParticle::ParticleSoAFloatPrecision fY = storage.operator()<KokkosParticle::AttributeNames::forceY, true, forEachHostFlag>(i);
+                typename KokkosParticle::ParticleSoAFloatPrecision fZ = storage.operator()<KokkosParticle::AttributeNames::forceZ, true, forEachHostFlag>(i);
 
                 storage.operator()<KokkosParticle::AttributeNames::oldForceX, true, forEachHostFlag>(i) = fX;
                 storage.operator()<KokkosParticle::AttributeNames::oldForceY, true, forEachHostFlag>(i) = fY;
@@ -85,9 +85,9 @@ int main(int argc, char** argv) {
                 fY *= (deltaT * deltaT / (2 * mass));
                 fZ *= (deltaT * deltaT / (2 * mass));
 
-                const auto displacementX = vX + fX;
-                const auto displacementY = vY + fY;
-                const auto displacementZ = vZ + fZ;
+                const typename KokkosParticle::ParticleSoAFloatPrecision displacementX = vX + fX;
+                const typename KokkosParticle::ParticleSoAFloatPrecision displacementY = vY + fY;
+                const typename KokkosParticle::ParticleSoAFloatPrecision displacementZ = vZ + fZ;
 
                 storage.operator()<KokkosParticle::AttributeNames::posX, true, forEachHostFlag>(i) = displacementX + storage.operator()<KokkosParticle::AttributeNames::posX, true, forEachHostFlag>(i);
                 storage.operator()<KokkosParticle::AttributeNames::posY, true, forEachHostFlag>(i) = displacementY + storage.operator()<KokkosParticle::AttributeNames::posY, true, forEachHostFlag>(i);
@@ -116,19 +116,19 @@ int main(int argc, char** argv) {
             Kokkos::Profiling::pushRegion("Velocity Update");
             autoPasInstance.forEachKokkos<ForEachSpace::execution_space>(KOKKOS_LAMBDA(int i, const autopas::utils::KokkosStorage<KokkosParticle>& storage) {
 
-                const auto mass = storage.operator()<KokkosParticle::AttributeNames::mass, true, forEachHostFlag>(i);
+                const typename KokkosParticle::ParticleSoAFloatPrecision mass = storage.operator()<KokkosParticle::AttributeNames::mass, true, forEachHostFlag>(i);
 
-                const auto fX = storage.operator()<KokkosParticle::AttributeNames::forceX, true, forEachHostFlag>(i);
-                const auto fY = storage.operator()<KokkosParticle::AttributeNames::forceY, true, forEachHostFlag>(i);
-                const auto fZ = storage.operator()<KokkosParticle::AttributeNames::forceZ, true, forEachHostFlag>(i);
+                const typename KokkosParticle::ParticleSoAFloatPrecision fX = storage.operator()<KokkosParticle::AttributeNames::forceX, true, forEachHostFlag>(i);
+                const typename KokkosParticle::ParticleSoAFloatPrecision fY = storage.operator()<KokkosParticle::AttributeNames::forceY, true, forEachHostFlag>(i);
+                const typename KokkosParticle::ParticleSoAFloatPrecision fZ = storage.operator()<KokkosParticle::AttributeNames::forceZ, true, forEachHostFlag>(i);
 
-                const auto oldFx = storage.operator()<KokkosParticle::AttributeNames::oldForceX, true, forEachHostFlag>(i);
-                const auto oldFy = storage.operator()<KokkosParticle::AttributeNames::oldForceY, true, forEachHostFlag>(i);
-                const auto oldFz = storage.operator()<KokkosParticle::AttributeNames::oldForceZ, true, forEachHostFlag>(i);
+                const typename KokkosParticle::ParticleSoAFloatPrecision oldFx = storage.operator()<KokkosParticle::AttributeNames::oldForceX, true, forEachHostFlag>(i);
+                const typename KokkosParticle::ParticleSoAFloatPrecision oldFy = storage.operator()<KokkosParticle::AttributeNames::oldForceY, true, forEachHostFlag>(i);
+                const typename KokkosParticle::ParticleSoAFloatPrecision oldFz = storage.operator()<KokkosParticle::AttributeNames::oldForceZ, true, forEachHostFlag>(i);
 
-                const auto vUpdateX = (fX + oldFx) * (deltaT / (2 * mass));
-                const auto vUpdateY = (fY + oldFy) * (deltaT / (2 * mass));
-                const auto vUpdateZ = (fZ + oldFz) * (deltaT / (2 * mass));
+                const typename KokkosParticle::ParticleSoAFloatPrecision vUpdateX = (fX + oldFx) * (deltaT / (2 * mass));
+                const typename KokkosParticle::ParticleSoAFloatPrecision vUpdateY = (fY + oldFy) * (deltaT / (2 * mass));
+                const typename KokkosParticle::ParticleSoAFloatPrecision vUpdateZ = (fZ + oldFz) * (deltaT / (2 * mass));
 
                 storage.operator()<KokkosParticle::AttributeNames::velocityX, true, forEachHostFlag>(i) = vUpdateX + storage.operator()<KokkosParticle::AttributeNames::velocityX, true, forEachHostFlag>(i);
                 storage.operator()<KokkosParticle::AttributeNames::velocityY, true, forEachHostFlag>(i) = vUpdateY + storage.operator()<KokkosParticle::AttributeNames::velocityX, true, forEachHostFlag>(i);
