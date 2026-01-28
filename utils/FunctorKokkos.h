bb5aaa952754e7df8746e78789f6650e86e20536
Luis Gall <luis.gall@tum.de>
2026-01-28 14:38:22 +0100

    Explicitly static type instead of auto

--- utils/FunctorKokkos.h
+++ utils/FunctorKokkos.h
@@ -36,7 +36,7 @@ public:
     void SoAFunctorSingleKokkos(const Particle_T::KokkosSoAArraysType& soa, bool newton3) final {
 
         const size_t N = soa.size();
-        const auto cutoffSquared = static_cast<typename Particle_T::ParticleSoAFloatPrecision>(_cutoffSquared);
+        const typename Particle_T::ParticleSoAFloatPrecision cutoffSquared = static_cast<typename Particle_T::ParticleSoAFloatPrecision>(_cutoffSquared);
 
         //Kokkos::parallel_for(Kokkos::TeamPolicy<typename MemSpace::execution_space>(N, Kokkos::AUTO()), KOKKOS_LAMBDA(Kokkos::TeamPolicy<typename MemSpace::execution_space>::member_type team) {
         Kokkos::parallel_for(Kokkos::RangePolicy<typename MemSpace::execution_space>(0, N), KOKKOS_LAMBDA(int i) {
@@ -49,9 +49,9 @@ public:
                 typename Particle_T::ParticleSoAFloatPrecision fyAcc = 0.;
                 typename Particle_T::ParticleSoAFloatPrecision fzAcc = 0.;
 
-                const auto x1 = soa.template operator()<Particle_T::AttributeNames::posX, true, false>(i);
-                const auto y1 = soa.template operator()<Particle_T::AttributeNames::posY, true, false>(i);
-                const auto z1 = soa.template operator()<Particle_T::AttributeNames::posZ, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision x1 = soa.template operator()<Particle_T::AttributeNames::posX, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision y1 = soa.template operator()<Particle_T::AttributeNames::posY, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision z1 = soa.template operator()<Particle_T::AttributeNames::posZ, true, false>(i);
 
                 for (int j = 0; j < N; ++j) {
                     if (i != j) {
@@ -59,35 +59,35 @@ public:
                         const auto owned2 = soa.template operator()<Particle_T::AttributeNames::ownershipState, true, false>(j);
 
                         if (owned2 != autopas::OwnershipState::dummy) {
-                            const auto x2 = soa.template operator()<Particle_T::AttributeNames::posX, true, false>(j);
-                            const auto y2 = soa.template operator()<Particle_T::AttributeNames::posY, true, false>(j);
-                            const auto z2 = soa.template operator()<Particle_T::AttributeNames::posZ, true, false>(j);
+                            const typename Particle_T::ParticleSoAFloatPrecision x2 = soa.template operator()<Particle_T::AttributeNames::posX, true, false>(j);
+                            const typename Particle_T::ParticleSoAFloatPrecision y2 = soa.template operator()<Particle_T::AttributeNames::posY, true, false>(j);
+                            const typename Particle_T::ParticleSoAFloatPrecision z2 = soa.template operator()<Particle_T::AttributeNames::posZ, true, false>(j);
 
-                            const auto drX = x1 - x2;
-                            const auto drY = y1 - y2;
-                            const auto drZ = z1 - z2;
+                            const typename Particle_T::ParticleSoAFloatPrecision drX = x1 - x2;
+                            const typename Particle_T::ParticleSoAFloatPrecision drY = y1 - y2;
+                            const typename Particle_T::ParticleSoAFloatPrecision drZ = z1 - z2;
 
-                            const auto drX2 = drX * drX;
-                            const auto drY2 = drY * drY;
-                            const auto drZ2 = drZ * drZ;
+                            const typename Particle_T::ParticleSoAFloatPrecision drX2 = drX * drX;
+                            const typename Particle_T::ParticleSoAFloatPrecision drY2 = drY * drY;
+                            const typename Particle_T::ParticleSoAFloatPrecision drZ2 = drZ * drZ;
 
-                            const auto dr2 = drX2 + drY2 + drZ2;
+                            const typename Particle_T::ParticleSoAFloatPrecision dr2 = drX2 + drY2 + drZ2;
 
                             if (dr2 <= cutoffSquared) {
                                 // TODO: consider mixing based on type or some sort of parameter injection
                                 const typename Particle_T::ParticleSoAFloatPrecision sigmaSquared = 1.;
                                 const typename Particle_T::ParticleSoAFloatPrecision epsilon24 = 24.;
 
-                                const auto invDr2 = 1.0 / dr2;
-                                auto lj6 = sigmaSquared * invDr2;
+                                const typename Particle_T::ParticleSoAFloatPrecision invDr2 = 1.0 / dr2;
+                                typename Particle_T::ParticleSoAFloatPrecision lj6 = sigmaSquared * invDr2;
                                 lj6 = lj6 * lj6 * lj6;
-                                const auto lj12 = lj6 * lj6;
-                                const auto lj12m6 = lj12 - lj6;
-                                const auto fac = epsilon24 * (lj12 + lj12m6) * invDr2;
+                                const typename Particle_T::ParticleSoAFloatPrecision lj12 = lj6 * lj6;
+                                const typename Particle_T::ParticleSoAFloatPrecision lj12m6 = lj12 - lj6;
+                                const typename Particle_T::ParticleSoAFloatPrecision fac = epsilon24 * (lj12 + lj12m6) * invDr2;
 
-                                const auto fX = fac * drX;
-                                const auto fY = fac * drY;
-                                const auto fZ = fac * drZ;
+                                const typename Particle_T::ParticleSoAFloatPrecision fX = fac * drX;
+                                const typename Particle_T::ParticleSoAFloatPrecision fY = fac * drY;
+                                const typename Particle_T::ParticleSoAFloatPrecision fZ = fac * drZ;
 
                                 // TODO: consider newton3 if enabled
                                 fxAcc += fX;
@@ -100,13 +100,13 @@ public:
                 //}, fxAcc, fyAcc, fzAcc);
                 //team.team_barrier();
 
-                const auto oldFx = soa.template operator()<Particle_T::AttributeNames::forceX, true, false>(i);
-                const auto oldFy = soa.template operator()<Particle_T::AttributeNames::forceY, true, false>(i);
-                const auto oldFz = soa.template operator()<Particle_T::AttributeNames::forceZ, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision oldFx = soa.template operator()<Particle_T::AttributeNames::forceX, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision oldFy = soa.template operator()<Particle_T::AttributeNames::forceY, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision oldFz = soa.template operator()<Particle_T::AttributeNames::forceZ, true, false>(i);
 
-                const auto newFx = oldFx + fxAcc;
-                const auto newFy = oldFy + fyAcc;
-                const auto newFz = oldFz + fzAcc;
+                const typename Particle_T::ParticleSoAFloatPrecision newFx = oldFx + fxAcc;
+                const typename Particle_T::ParticleSoAFloatPrecision newFy = oldFy + fyAcc;
+                const typename Particle_T::ParticleSoAFloatPrecision newFz = oldFz + fzAcc;
 
                 soa.template operator()<Particle_T::AttributeNames::forceX, true, false>(i) = newFx;
                 soa.template operator()<Particle_T::AttributeNames::forceY, true, false>(i) = newFy;
@@ -119,7 +119,7 @@ public:
         const size_t N = soa1.size();
         const size_t M = soa2.size();
 
-        const auto cutoffSquared = static_cast<typename Particle_T::ParticleSoAFloatPrecision>(_cutoffSquared);
+        const typename Particle_T::ParticleSoAFloatPrecision cutoffSquared = static_cast<typename Particle_T::ParticleSoAFloatPrecision>(_cutoffSquared);
 
         //Kokkos::TeamPolicy<typename MemSpace::execution_space> policy (N, Kokkos::AUTO());
 
@@ -133,43 +133,43 @@ public:
                 typename Particle_T::ParticleSoAFloatPrecision fyAcc = 0.;
                 typename Particle_T::ParticleSoAFloatPrecision fzAcc = 0.;
 
-                const auto x1 = soa1.template operator()<Particle_T::AttributeNames::posX, true, false>(i);
-                const auto y1 = soa1.template operator()<Particle_T::AttributeNames::posY, true, false>(i);
-                const auto z1 = soa1.template operator()<Particle_T::AttributeNames::posZ, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision x1 = soa1.template operator()<Particle_T::AttributeNames::posX, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision y1 = soa1.template operator()<Particle_T::AttributeNames::posY, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision z1 = soa1.template operator()<Particle_T::AttributeNames::posZ, true, false>(i);
 
                 for (int j = 0; j < M; ++j) {
                     const auto owned2 = soa2.template operator()<Particle_T::AttributeNames::ownershipState, true, false>(j);
 
                     if (owned2 != autopas::OwnershipState::dummy) {
-                        const auto x2 = soa2.template operator()<Particle_T::AttributeNames::posX, true, false>(j);
-                        const auto y2 = soa2.template operator()<Particle_T::AttributeNames::posY, true, false>(j);
-                        const auto z2 = soa2.template operator()<Particle_T::AttributeNames::posZ, true, false>(j);
+                        const typename Particle_T::ParticleSoAFloatPrecision x2 = soa2.template operator()<Particle_T::AttributeNames::posX, true, false>(j);
+                        const typename Particle_T::ParticleSoAFloatPrecision y2 = soa2.template operator()<Particle_T::AttributeNames::posY, true, false>(j);
+                        const typename Particle_T::ParticleSoAFloatPrecision z2 = soa2.template operator()<Particle_T::AttributeNames::posZ, true, false>(j);
 
-                        const auto drX = x1 - x2;
-                        const auto drY = y1 - y2;
-                        const auto drZ = z1 - z2;
+                        const typename Particle_T::ParticleSoAFloatPrecision drX = x1 - x2;
+                        const typename Particle_T::ParticleSoAFloatPrecision drY = y1 - y2;
+                        const typename Particle_T::ParticleSoAFloatPrecision drZ = z1 - z2;
 
-                        const auto drX2 = drX * drX;
-                        const auto drY2 = drY * drY;
-                        const auto drZ2 = drZ * drZ;
+                        const typename Particle_T::ParticleSoAFloatPrecision drX2 = drX * drX;
+                        const typename Particle_T::ParticleSoAFloatPrecision drY2 = drY * drY;
+                        const typename Particle_T::ParticleSoAFloatPrecision drZ2 = drZ * drZ;
 
-                        const auto dr2 = drX2 + drY2 + drZ2;
+                        const typename Particle_T::ParticleSoAFloatPrecision dr2 = drX2 + drY2 + drZ2;
 
                         if (dr2 <= cutoffSquared) {
                             // TODO: consider mixing based on type or some sort of parameter injection
                             const typename Particle_T::ParticleSoAFloatPrecision sigmaSquared = 1.;
                             const typename Particle_T::ParticleSoAFloatPrecision epsilon24 = 24.;
 
-                            const auto invDr2 = 1.0 / dr2;
-                            auto lj6 = sigmaSquared * invDr2;
+                            const typename Particle_T::ParticleSoAFloatPrecision invDr2 = 1.0 / dr2;
+                            typename Particle_T::ParticleSoAFloatPrecision lj6 = sigmaSquared * invDr2;
                             lj6 = lj6 * lj6 * lj6;
-                            const auto lj12 = lj6 * lj6;
-                            const auto lj12m6 = lj12 - lj6;
-                            const auto fac = epsilon24 * (lj12 + lj12m6) * invDr2;
+                            const typename Particle_T::ParticleSoAFloatPrecision lj12 = lj6 * lj6;
+                            const typename Particle_T::ParticleSoAFloatPrecision lj12m6 = lj12 - lj6;
+                            const typename Particle_T::ParticleSoAFloatPrecision fac = epsilon24 * (lj12 + lj12m6) * invDr2;
 
-                            const auto fX = fac * drX;
-                            const auto fY = fac * drY;
-                            const auto fZ = fac * drZ;
+                            const typename Particle_T::ParticleSoAFloatPrecision fX = fac * drX;
+                            const typename Particle_T::ParticleSoAFloatPrecision fY = fac * drY;
+                            const typename Particle_T::ParticleSoAFloatPrecision fZ = fac * drZ;
 
                             // TODO: consider newton3 if enabled
                             fxAcc += fX;
@@ -180,13 +180,13 @@ public:
                 //}, fxAcc, fyAcc, fzAcc);
                 }
 
-                const auto oldFx = soa1.template operator()<Particle_T::AttributeNames::forceX, true, false>(i);
-                const auto oldFy = soa1.template operator()<Particle_T::AttributeNames::forceY, true, false>(i);
-                const auto oldFz = soa1.template operator()<Particle_T::AttributeNames::forceZ, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision oldFx = soa1.template operator()<Particle_T::AttributeNames::forceX, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision oldFy = soa1.template operator()<Particle_T::AttributeNames::forceY, true, false>(i);
+                const typename Particle_T::ParticleSoAFloatPrecision oldFz = soa1.template operator()<Particle_T::AttributeNames::forceZ, true, false>(i);
 
-                const auto newFx = oldFx + fxAcc;
-                const auto newFy = oldFy + fyAcc;
-                const auto newFz = oldFz + fzAcc;
+                const typename Particle_T::ParticleSoAFloatPrecision newFx = oldFx + fxAcc;
+                const typename Particle_T::ParticleSoAFloatPrecision newFy = oldFy + fyAcc;
+                const typename Particle_T::ParticleSoAFloatPrecision newFz = oldFz + fzAcc;
 
                 soa1.template operator()<Particle_T::AttributeNames::forceX, true, false>(i) = newFx;
                 soa1.template operator()<Particle_T::AttributeNames::forceY, true, false>(i) = newFy;
